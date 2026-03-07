9996e8e6a308977df9bc2b7bfada721baf787ef5
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-03-07 20:51:58 +0000

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,17 @@
 require("dotenv").config();
 
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-if (process.env.DB_PASSWORD === "ChangeMe!") {
-  console.error("Please update the .env file with your database password.");
-  process.exit(1);
-}
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_DATABASE,
-      process.env.DB_USERNAME,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
