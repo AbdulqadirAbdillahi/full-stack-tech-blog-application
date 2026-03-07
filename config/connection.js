415eb0f90970c1ef47c87ed135fedb268c0adb96
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-03-07 21:20:50 +0000

    connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
-
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  logging: false,
-});
+// Use DATABASE_URL if on Render (external DB URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        port: process.env.DB_PORT || 5432,
+        logging: false,
+      }
+    );
 
 module.exports = sequelize;
